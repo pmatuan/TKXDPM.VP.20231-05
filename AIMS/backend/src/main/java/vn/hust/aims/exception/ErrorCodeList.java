8d6e966497409165d88c0f8495716e09f3f3ec40
milo-v <miloygo1811@gmail.com>
2023-12-28 21:03:30 +0700

    backend for user login function

--- AIMS/backend/src/main/java/vn/hust/aims/exception/ErrorCodeList.java
+++ AIMS/backend/src/main/java/vn/hust/aims/exception/ErrorCodeList.java
@@ -24,7 +24,10 @@ public enum ErrorCodeList {
   NULL_EMAIL("EMAIL_001", "Email must not be null"),
   NULL_PASSWORD("PASSWORD_001", "Password must not be null"),
   INVALID_ROLE("ROLE_001", "User role has not been set or is invalid"),
-  INVALID_BLOCKED_STATE("BLOCKED_STATE_001", "setIsBlocked value must be 0 or 1")
+  INVALID_BLOCKED_STATE("BLOCKED_STATE_001", "setIsBlocked value must be 0 or 1"),
+  EMAIL_NOT_FOUND("LOGIN_001", "email not found"),
+  WRONG_PASSWORD("LOGIN_002", "wrong password"),
+  EMAIL_EXISTS("EMAIL_002", "Cannot create user: email already exists")
   ;
 
 
--- AIMS/backend/src/main/java/vn/hust/aims/repository/user/UserRepository.java
+++ AIMS/backend/src/main/java/vn/hust/aims/repository/user/UserRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.hust.aims.entity.user.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @NotNull Page<User> findAll(Pageable pageable);
+
+    public Optional<User> findByEmail(String email);
 }
